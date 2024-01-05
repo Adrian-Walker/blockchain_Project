913ddad59c1b96d52fff986093a4be4da2d9458e
Adrian-Walker <awalker3365@gmail.com>
2024-01-04 19:55:20 -0500

    added new code

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,16 @@
-blockchain = [100]
+blockchain = []
 
 
 def get_last_value():
     return blockchain[-1]
 
 
-def add_one(t):
-    blockchain.append([get_last_value(), t])
+def add_one(transaction, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction])
 
 
 add_one(200)
-add_one(300)
-add_one(400)
+add_one(300, get_last_value())
+add_one(400, get_last_value())
 
 print(blockchain)
